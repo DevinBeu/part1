fe488e4cd472f1426e303e182e49308ae983342b
Devin Beu <devinbeu@gmail.com>
2023-08-25 10:15:40 -0400

    destructure content prop

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,20 @@ const Header = (props) => {
   return <h1>{props.text}</h1>;
 };
 
-const Part = (props) => {
-  console.log(props);
+const Part = ({ name, exercises }) => {
   return (
     <p>
-      {props.name} {props.exercises}
+      {name} {exercises}
     </p>
   );
 };
 
-const Content = (props) => {
-  console.log(props);
+const Content = ({ part1, part2, part3 }) => {
   return (
     <div>
-      <Part name={props.part1.name} exercises={props.part1.exercises} />
-      <Part name={props.part2.name} exercises={props.part2.exercises} />
-      <Part name={props.part3.name} exercises={props.part3.exercises} />
+      <Part name={part1.name} exercises={part1.exercises} />
+      <Part name={part2.name} exercises={part2.exercises} />
+      <Part name={part3.name} exercises={part3.exercises} />
     </div>
   );
 };
